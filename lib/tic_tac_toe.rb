ef8999991ece1279c20d13d8a1efd8a0933e1fbd
Laura Radville <laura.radville@gmail.com>
2019-07-25 13:26:16 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,70 @@
+require "pry"
+
+class TicTacToe
+  def initialize
+    @board = Array.new(9, " ")
+  end
+  
+  WIN_COMBINATIONS = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+    ]
+    
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+  
+  def input_to_index(location)
+    location.to_i - 1.to_i
+  end
+  
+  def move(index, token = "X")
+    @board[index] = token
+  end
+  
+  def position_taken?(index)
+    @board[index].include?("X") || @board[index].include?("O")
+  end
+  
+  def valid_move?(position)
+    if position < 9 && !position_taken?(position)
+      true
+    end
+  end
+  
+  def turn_count
+    @board.count{|token| token == "X" || token == "O"}
+  end
+  
+  def current_player
+    if turn_count.odd?
+      "O"
+    else
+      "X"
+    end
+  end
+  
+  def turn
+    position = 11
+    display_board
+        # binding.pry
+
+    until valid_move?(position)
+      puts "Choose a position between 1-9."
+      index = gets.chomp
+      position = input_to_index(index)
+    end
+    @board[position] = current_player
+  end
+  
+end
