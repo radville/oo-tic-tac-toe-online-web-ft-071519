0f0aec6e92d5d93d320199a485ce3abf11be37ac
Laura Radville <laura.radville@gmail.com>
2019-07-25 14:27:39 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -62,9 +62,10 @@ class TicTacToe
     index = gets.strip
     position = input_to_index(index)
     if valid_move?(position)
+      binding.pry
       @board[position] = current_player
     else
-                binding.pry
+      # binding.pry
 
       turn
     end
