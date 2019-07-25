834538d32347b8b4d9d61cc033a7df1a09b97376
Laura Radville <laura.radville@gmail.com>
2019-07-25 17:54:11 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -68,11 +68,12 @@ class TicTacToe
     WIN_COMBINATIONS.each do |win|
       if win.map {|el| @board[el] == "X" }.all?
         return win
-        binding.pry
       elsif win.map {|el| @board[el] == "O" }.all?
         return win
       else
         nil
+                binding.pry
+
       end
     end
   end
